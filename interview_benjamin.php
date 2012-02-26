44cca0d5c7bcd31d458a5e578b8b4d2b8fd01c42
Benjamin Fisher <neohell@hotmail.com>
2012-02-25 17:44:34 -0800

    Added videos to assets/about. Removed the global sidebar for individual pages and added it to the header file (minor correction to global css to accomodate).

--- interview_benjamin.php
+++ interview_benjamin.php
@@ -15,47 +15,6 @@ include 'header.php';
 			</video>
 			
 		</section>
-
-		<!-- global sidebar -->
-
-		<aside class="sidebar">
-			<section class="news group">
-				<h3>Latest news</h3>
-
-				<article>
-					<details>
-						<summary>
-							<strong>New Office</strong>
-							<time datetime="2011-01-20">Jan 20th, 2011</time>
-						</summary>
-						<p>
-							The dust is cleared and we finally settled into our new office in
-							downtown Portland. Be sure to come by and see our new digs. We even
-							have fancy vending machines, an orange accent wall, and a pool table.
-						</p>
-					</details>
-				</article>
-
-				<article>
-					<details>
-						<summary>
-							<strong>We Won a Medal!</strong>
-							<time datetime="2011-12-14">Dec. 14th, 2011</time>
-						</summary>
-						<p>
-							The team recently won a WebDev 2012 Gold Medal for our pioneering
-							designs using HTML5. We're very proud of this! Check out our portfolio
-							to see some of the work that lead to this award.
-						</p>
-					</details>
-				</article>
-			</section>
-
-			<section class="did-you-know group">
-				<h3>Did You Know?</h3>
-				<p>Charlie has an uncle named OJ. Enough said.</p>
-			</section>
-		</aside>
 	</section>
 
 <?php include 'footer.php'; ?>
