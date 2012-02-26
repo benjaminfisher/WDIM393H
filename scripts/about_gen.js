a7c4a70a526c96d40e709fd37a33bd06827721f3
Benjamin Fisher <neohell@hotmail.com>
2012-02-25 22:44:29 -0800

    Added more of the factoids to about_gen.js.

--- scripts/about_gen.js
+++ scripts/about_gen.js
@@ -6,8 +6,11 @@ $(function(){
 	var content = [
 		"Charlie has an uncle named OJ. Enough said.",
 		"April loves ferrets. She has owned over 25 in her life time and has 8 of her ferrets tattooed on her body.",
+		"April can bellydance. She started dancing back in 1997.",
 		"Benjamin does the poetry thing. He produces and hosts a monthly reading series featuring local open mike poets.",
-		"Nate was named after Nathaniel Hawthorne."
+		"Benjamin can juggle. He currently juggles up to 4 balls, 3 clubs, and contact juggles.",
+		"Nate was named after Nathaniel Hawthorne.",
+		"Nate speaks four languages: English, French, Spanish and Italian. On a good day, he speaks a tiny bit of Korean, Greek and Japanese too."
 	];
 	
 	$("#about").text(content[Math.floor(Math.random() * (content.length))]);
