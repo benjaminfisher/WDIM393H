6c15af1cc07931b1a557e12c38a0f0cfdf78c8bf
Benjamin Fisher <neohell@hotmail.com>
2012-02-25 22:52:46 -0800

    About text correction.

--- scripts/about_gen.js
+++ scripts/about_gen.js
@@ -9,7 +9,7 @@ $(function(){
 		"April can bellydance. She started dancing back in 1997.",
 		"Benjamin does the poetry thing. He produces and hosts a monthly reading series featuring local open mike poets.",
 		"Benjamin can juggle. He currently juggles up to 4 balls, 3 clubs, and contact juggles.",
-		"Nate was named after Nathaniel Hawthorne.",
+		"Nate was named after one of the earliest authors of American literature, Nathaniel Hawthorne.",
 		"Nate speaks four languages: English, French, Spanish and Italian. On a good day, he speaks a tiny bit of Korean, Greek and Japanese too."
 	];
 	
