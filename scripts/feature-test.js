3663c68aed8fb9954f285adfcb04f195c9c87159
Benjamin Fisher <neohell@hotmail.com>
2012-02-27 18:02:29 -0800

    Fixed details better

--- scripts/feature-test.js
+++ scripts/feature-test.js
@@ -4,7 +4,11 @@ $(function(){
 			nope: ['scripts/progress-polyfill.min.js', 'css/progress-polyfill.css'],
 		},{
 			test: Modernizr.details,
-			nope: ['css/details.css','scripts/jquery.details.min.js', 'scripts/details_run.js']
+			nope: ['css/details.css','scripts/jquery.details.min.js'],
+			callback: function(url, result, key){
+				console.log(url, result, key);
+				if(!result && key == 1) $('details').details();
+			}
 		}
 	]);
 })
