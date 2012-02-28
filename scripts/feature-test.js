c9544ea0ea7cafb8bd61511c2a9ad485d68b7bf5
Benjamin Fisher <neohell@hotmail.com>
2012-02-27 17:52:43 -0800

    finally fixed the freaking feature test script

--- scripts/feature-test.js
+++ scripts/feature-test.js
@@ -4,8 +4,7 @@ $(function(){
 			nope: ['scripts/progress-polyfill.min.js', 'css/progress-polyfill.css'],
 		},{
 			test: Modernizr.details,
-			nope: ['css/details.css', 'scripts/jquery.details.min.js'],
-			complete: function() {$('details').details()}
+			nope: ['css/details.css','scripts/jquery.details.min.js', 'scripts/details_run.js']
 		}
 	]);
 })
